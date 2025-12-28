0cdd3cc7efe201817f29fafe2c85a2b795e086be
Rohit Kumar <125601198+rohitkumar3848@users.noreply.github.com>
2025-12-28 20:15:03 +0530

    SpringBootJPA basic CRUD and REST API related code pushed

---
+++ SringBootJPA01/src/main/java/com/cfs/SringBootJPA01/SringBootJpa01Application.java
@@ -0,0 +1,13 @@
+package com.cfs.SringBootJPA01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringBootJpa01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringBootJpa01Application.class, args);
+	}
+
+}
